7c6fad8b4f35ef432620c3a4707be968ef60511c
Samy Amraoui <sametzy@gmail.com>
2024-05-13 20:36:32 +0200

    feat: add support for python and typescript languages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,6 +13,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
     { import = "plugins" },
   },
